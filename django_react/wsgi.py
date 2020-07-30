4fba4db357c32e34e06189593817fdefd8a517ac
Sergei Sergienko <sergey.s.sergienko@gmail.com>
2020-07-31 01:34:04 +0300

    - wsgi

--- django_react/wsgi.py
+++ django_react/wsgi.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append('/opt/bitnami/apps/django/django_projects/ssergienko.com')
+sys.path.append('/opt/bitnami/apps/django/django_projects/ssergienko.com/frontend')
 os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/ssergienko.com/egg_cache")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_react.settings")
 from django.core.wsgi import get_wsgi_application
