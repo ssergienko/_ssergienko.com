d1f04536565a2a08114ab9ac17ddd7d2f57ea87c
Sergei Sergienko <sergey.s.sergienko@gmail.com>
2020-07-31 00:00:26 +0300

    - apache settings

--- django_react/wsgi.py
+++ django_react/wsgi.py
@@ -1,16 +1,7 @@
-"""
-WSGI config for settings project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
+sys.path.append('/opt/bitnami/apps/django/django_projects/settings.com')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/settings.com/egg_cache")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.settings")
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.settings')
-
 application = get_wsgi_application()
